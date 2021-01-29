d3e0137790b65f97a19721cd6a633b5c226ea92e
Hammy <sgoudham@gmail.com>
2021-01-29 02:56:41 +0000

    Rename handler to event_handler()

--- twitter/bot/handler.py
+++ twitter/bot/handler.py
@@ -1,6 +1,6 @@
 from twitter.bot.winston import Winston
 
 
-def handler(event, context):
+def event_handler(event, context):
     """Sends random tweet from list of potential tweets"""
     Winston.request_handler()
